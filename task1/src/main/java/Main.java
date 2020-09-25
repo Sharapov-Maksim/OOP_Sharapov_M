bc1afb2044d9f11c78a8715d2fc2e98923799ee4
Sharapov-Maksim <55018975+Sharapov-Maksim@users.noreply.github.com>
2020-09-25 13:52:34 +0700

    Update Main.java
    Изменён публичный контракт и поправлен стиль кода. Тесты пока не исправлены.

--- task1/src/main/java/Main.java
+++ task1/src/main/java/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main (String args[]) {
         int arr[] = {15, 4, -1, 3, 100, 100500, -123, 5, 5, 5, 18, -404, 505, 112};
-        Heap myHeap = new Heap(arr.length);
-        myHeap.heapsort(arr);
+        sort(arr);
         for (int i = 0; i < arr.length; i++){
             System.out.println(arr[i]);
         }
+    }
+    public static int[] sort (int []arr){
+        Heap.heapsort(arr);
+        return arr;
     }
 }
 
@@ -14,16 +17,16 @@ class Heap {
     int heap[];
     int lstid = 0;
 
-    public Heap(int size){
+    private Heap(int size){
         heap = new int[size];
     }
-    public void swap (int id1, int id2){
+    private void swap (int id1, int id2){
         int tmp = heap[id1];
         heap[id1] = heap[id2];
         heap[id2] = tmp;
     }
 
-    public void siftUp(int v){
+    private void siftUp(int v){
         if (v != 0){
             int f = (v - 1) / 2; //parent element
             if (heap[v]<heap[f]) {
@@ -33,44 +36,54 @@ class Heap {
         }
     }
 
-    public void add (int X){
+    private void add (int X){
         heap[lstid++] = X;
         siftUp(lstid-1);
     }
 
-    public int minID(int a, int b, int ida, int idb) {
+    private int minID(int a, int b, int ida, int idb) {
         return (a < b ? ida : idb);
     }
 
-    public void siftDown(int v){
+    private void siftDown(int v){
         int ls = v * 2 + 1; //left son
         int rs = v * 2 + 2; //right son
-        if (ls >= lstid) return;
-        if ((rs == lstid) && (heap[ls]>=heap[v])) return;
+        if (ls >= lstid) {
+            return;
+        }
+        if ((rs == lstid) && (heap[ls]>=heap[v])) {
+            return;
+        }
         if ((rs == lstid) && (heap[ls]<heap[v])) {
             swap(v, ls);
             return;
         }
-        if ((rs < lstid) && (heap[ls] >= heap[v]) && (heap[rs] >= heap[v])) return;
+        if ((rs < lstid) && (heap[ls] >= heap[v]) && (heap[rs] >= heap[v])) {
+            return;
+        }
         int minimal = minID(heap[ls], heap[rs], ls, rs);
         swap(v, minimal);
         siftDown(minimal);
     }
 
-    public int extractmin(){
+    private int extractmin(){
         int res = heap[0];
         swap(0, --lstid);
-        if (lstid!=0) siftDown(0);
+        if (lstid!=0) {
+            siftDown(0);
+        }
         return res;
 
     }
 
-    public void heapsort(int arr[]){
+    public static void heapsort(int arr[]){
+        Heap myHeap = new Heap(arr.length);
+
         for (int i = 0; i < arr.length; i++){
-            add(arr[i]);
+            myHeap.add(arr[i]);
         }
         for (int i = 0; i < arr.length; i++){
-            arr[i] = extractmin();
+            arr[i] = myHeap.extractmin();
         }
     }
 
